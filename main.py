b215dad61c578b9567c3a5ac80673f1c2c3afdaa
Om Thakur <thakurom0110@gmail.com>
2025-02-19 08:24:37 -0500

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mlopsProject import logger
 from src.mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlopsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlopsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -32,3 +33,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
