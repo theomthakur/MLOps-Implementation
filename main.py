557e4373b8059520bf78a67c08ec54b61ac15ea6
Om Thakur <thakurom0110@gmail.com>
2025-02-19 08:07:40 -0500

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlopsProject import logger
 from src.mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlopsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -18,6 +19,16 @@ try:
     obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
